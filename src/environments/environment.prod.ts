6d63ee43397e984759db501cbabd3ce5473a32e2
Michael Gruner <michael.gruner@gmail.com>
2020-04-13 08:31:16 +0000

    fix errors in environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: window["env"]["apiUrl"] || "http://localhost:8080"
+  apiBaseUrl: window["env"]["apiUrl"] || "default"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: window["env"]["adminioProd"] || false,
-  apiBaseUrl: window["env"]["apiUrl"] || "http://localhost:8080"
+  apiBaseUrl: window["env"]["apiUrl"] || "default"
 };
